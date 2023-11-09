587a5da5743e1a8d0e00770d05b7b1bbb3b971fd
Samael Hernandez <azull.samael@gmail.com>
2023-11-09 16:25:22 -0600

    Add project files.

---
+++ seccion2/seccion2/Program.cs
@@ -0,0 +1,31 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seccion2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre, apellido, dirreccion, correo, telefono, sexo;
+            int edad;
+            double estatura;
+            Boolean objeto;
+
+            nombre = "samael";
+            apellido = "Hernadez";
+            dirreccion = "carlos sagredo 2743";
+            correo = "ejemplo@gmail.com";
+            telefono = "449????????";
+            sexo = "Hombre";
+            edad = 27;
+            estatura = 1.75;
+            objeto = true;
+
+            Console.WriteLine(nombre + " " + apellido + " " + correo + " " + telefono + " " + sexo + " " + edad +" "+ dirreccion + " " +estatura + " " +objeto);
+        }
+    }
+}
