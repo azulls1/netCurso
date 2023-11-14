c1b5db6a978dfb528b8deab3db59f71a913c298d
azulls1 <azull.samael@gmail.com>
2023-11-13 22:40:35 -0600

    avance 13_11_2023

---
+++ seccion5_toma_de_decisiones/seccion5_toma_de_decisiones/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seccion5_toma_de_decisiones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+
+            Console.ReadKey();
+        }
+    }
+}
