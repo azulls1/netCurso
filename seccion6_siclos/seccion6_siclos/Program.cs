e52ff6f778e945b43d082a9a16fab84f4d2f46c0
Samael Hernandez <azull.samael@gmail.com>
2023-11-28 16:25:06 -0600

    avance 27/11/2023 nueva seccion  siclos(Bucles)

---
+++ seccion6_siclos/seccion6_siclos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//programa que  mostrara el usuo de los siclos 
+//for, while, do while
+namespace seccion6_siclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
