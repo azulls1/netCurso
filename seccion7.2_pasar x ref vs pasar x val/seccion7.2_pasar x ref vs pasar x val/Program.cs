ce68e41d714b20a23637f9d26873d0563adc4362
Samael Hernandez <azull.samael@gmail.com>
2024-03-22 16:14:54 -0600

    22/03/2024 nueva modificaciones

--- seccion7.2_pasar x ref vs pasar x val/seccion7.2_pasar x ref vs pasar x val/Program.cs
+++ seccion7.2_pasar x ref vs pasar x val/seccion7.2_pasar x ref vs pasar x val/Program.cs
@@ -10,6 +10,54 @@ namespace seccion7._2_pasar_x_ref_vs_pasar_x_val
     {
         static void Main(string[] args)
         {
+            /************** Inicio pasar por valor *********************
+
+            //variable local a main (argumento)
+            int numAr;
+            //asignamos un valor
+            numAr = 10;
+            //mostramos su valor en la consola
+            Console.WriteLine(numAr);
+            //invocamos al metodo y mandamos al argumento
+            prueba(numAr);
+            //despues de que el metodo cambio el valor, volvemos a mostrar al argumento
+            Console.WriteLine(numAr);
+
+           *************** Fin pasar por referencia *******************/
+           
+            //************************************************************************************************************
+            
+            //************* Inicio pasar por referencia****************
+
+            //variable local a main (argumento)
+            int numAr;
+            //asignamos un valor
+            numAr = 10;
+            //mostramos su valor en la consola
+            Console.WriteLine(numAr);
+            //invocamos al metodo y mandamos al argumento
+            prueba(ref numAr); // shace la declaracion de ref para mencionar que el arugmento fue modificado por el parametro
+            //despues de que el metodo cambio el valor, volvemos a mostrar al argumento
+            Console.WriteLine( numAr);
+
+            //************* Fin pasar por referencia****************
         }
+        /******************* metodo pasar por referencia
+        static void prueba( int numPa) 
+        {
+            //modifcamos el valor del parametro
+            numPa = 20;
+        }
+        *****************Fin metodo pasar por referecnia ***********************/
+
+        //***************************************************************************************************************
+
+        //************* Inicio metodo pasar por referencia****************
+        static void prueba( ref int numPa)// se tiene que poner antes ref para  que pueda ser moficado el valor y es bueno para tener varios valores y usarlos,  si usamos return solo regresa un valor  
+        {
+            //modifcamos el valor del parametro
+            numPa = 20;
+        }
+        //************* Fin metodo pasar por referencia****************
     }
 }
---
+++ seccion7.3_palabra_OUT/seccion7.3_palabra_OUT/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seccion7._3_palabra_OUT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            //variable local a main (argumento)
+            int numAr;
+            string textoAr;
+            double numDouAr;
+
+            //invocamos al metodo y mandamos a los argumentos
+            prueba(out numAr,out textoAr, out numDouAr); // shace la declaracion de ouy para mencionar que el arugmento fue modificado por el parametro
+           
+            //despues de que el metodo cambio el valor,  mostramos a los argumento
+            Console.WriteLine("valor numAr: {0} , valor textoAr: {1} , valor numDouAr:  {2}", numAr, textoAr, numDouAr);
+
+           
+        }
+        
+        static void prueba(out int numPa, out string textoPa, out double numDouPa)// se tiene que poner antes out para indicar que el argumento que fue pasado y guardado en parametro no estaba inicializado y a qui se modifico e incializo regresando el valor a main, se puede aplicar para varios valores
+        {
+            //modifcamos el valor de los parametro
+            numPa = 20;
+            textoPa = "texto prueba";
+            numDouPa = 7.5;
+        }
+        
+    }
+}
