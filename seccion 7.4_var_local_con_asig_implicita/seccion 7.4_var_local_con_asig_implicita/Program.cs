e95b910ee7c5ab974c9421161242acda2c5016d0
Samael Hernandez <azull.samael@gmail.com>
2024-03-25 15:26:04 -0600

    finalisacion de la seccion de metodos

---
+++ seccion 7.4_var_local_con_asig_implicita/seccion 7.4_var_local_con_asig_implicita/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seccion_7._4_var_local_con_asig_implicita // cariables locales con asignacion implcita de tipos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Al declarar una variable local, puede permitir que el compilador infiera el tipo de la variable a partir de la expresión de inicialización. Para ello, use la palabra clave var en lugar del nombre de un tipo
+
+            //Nota: al poner var no se declara el tipo de dato si no que tomara el que mas es adecuado dependiendo el valor que se asigne en este caso  como los siguientes ejemplos
+            var nombre = "luis";  //string
+            var calle = "av 5"; //string
+            var numero = 10; //int
+            var calificacion = 8.5; //double
+            var letra = 'A';  //char
+            var NUMERO2 = 6; //int
+            var campo = true; //bolean
+
+
+            // tuplas
+
+            /* https: //learn.microsoft.com/es-es/dotnet/csharp/language-reference/builtin-types/value-tuples 
+            Cuando nos referimos a una Tupla nos estamos refiriendo a una estructura de datos la cual puede contener múltiples tipos.
+            La característica de tuplas proporciona una sintaxis concisa para agrupar varios elementos de datos en una estructura de datos ligera*/
+
+
+            //forma de declarar una tupla
+            //(Tipo) Identificador = (valor);
+
+            string nombre1 = "ashm";
+            int edad = 28;
+            long numeroTel = 4499999999;
+            double direccion = 2743.2;
+
+               (string, int, long, double) persona1 = ("ashm",28,4499999999,2743.2);//esta es la forma de declarar una tupla de una forma larga
+                var persona2 = ("ashm", 28, 4499999999, 2743.2);//version simplificada de una tupla
+            var persona3 = (nombre3 : "ashm", edad3 : 28, numero3 :  4499999999, dirrecion3 : 2743.2);//version simplificada de una tupla
+            Console.WriteLine(persona1);//imprimer la tupla
+
+            //imprimimos los items de la tupla
+
+            Console.WriteLine(persona2.Item1+ " (impresion por item) ");
+
+            //imprimimos los identificadores de la tupla
+            Console.WriteLine(persona3.nombre3+" "+ persona3.edad3 +" (impresion por identificador en tupla mas barios elementos de la tupla) ");
+
+
+
+
+
+
+        }
+    }
+}
---
+++ tarea seccion 7-/tarea seccion 7-/Program.cs
@@ -0,0 +1,240 @@
+﻿using System    ;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace tarea_seccion_7_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int OpcionRetornada;
+            OpcionRetornada=menuPrincipal();
+             
+            if (OpcionRetornada == 1)
+            {
+                conversionGraRad();
+            }
+            else
+            {
+                calcularArea();
+            }
+
+        }
+
+        static int menuPrincipal()
+        {
+            int opcion;
+            do
+            {
+
+                Console.WriteLine("Elige la opcion que deseas efectuar");
+                Console.WriteLine("1. transformar grados a radianes");
+                Console.WriteLine("2. calcular el area de una figura");
+                opcion = Convert.ToInt32(Console.ReadLine());
+            }
+            while((opcion < 0) || (opcion > 2));
+
+
+            return opcion;
+        }
+
+        static void conversionGraRad()
+        {
+            double grados, resultado;
+                     
+            Console.WriteLine("me puedes dar los grados que deseas converitr");
+            grados = Convert.ToDouble(Console.ReadLine());
+
+            resultado = (grados * Math.PI) / 180;
+
+            Console.WriteLine(resultado);              
+        }
+
+        static void calcularArea()
+        {
+            int opcion;
+            double area;
+            do
+            {
+                Console.WriteLine("elige la figura que deseas obtener el area");
+                Console.WriteLine("1 cuadrado ");
+                Console.WriteLine("2 ciruclo ");
+                Console.WriteLine("3 triangulo ");
+                opcion= Convert.ToInt32(Console.ReadLine());
+
+            } 
+            while ((opcion < 1) || (opcion > 3));
+             
+            switch (opcion) {
+                case 1:
+                    
+                   area = areaCuadrado();
+                   Console.WriteLine("el area del cuadrado es {0}",area);
+                 break;
+
+                case 2:
+
+                    area = areaCirculo();
+                    Console.WriteLine("el area del circulo es {0}", area);
+                    break;
+                case 3:
+
+                    area = areaTriangulo();
+                    Console.WriteLine("el area del circulo es {0}", area);
+                    break;
+
+
+
+
+            }
+
+        }
+
+        static double areaCuadrado()
+        {
+            double largo, ancho;
+            double resultado;
+            Console.WriteLine(" me puedes dar el largo del cuadrado");
+            largo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("me puedes darl el ancho del cuadrado");
+            ancho = Convert.ToDouble(Console.ReadLine());
+
+            resultado = largo * ancho;
+
+            return resultado;
+        }
+        static double areaCirculo()
+        {
+            double radio;
+            double resultado;
+            Console.WriteLine("me puedes dar el radio del circulo : ");
+            radio = Convert.ToDouble(Console.ReadLine());
+            
+            resultado= Math.PI * (Math.Pow(radio,2));
+
+            return resultado;
+        }
+
+       static double areaTriangulo()
+        {
+            double resultado;
+            double basee, altura;
+            Console.WriteLine("me puedes dar la base :");
+            basee = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("me puedes dal la altura");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            resultado = (basee * altura) / 2;
+
+
+            return resultado;
+        }
+
+
+
+
+
+
+    }//Fin de la clase Program
+}
+// como lo aria el profesor 
+/* calcular el area de las figuras
+ namespace Seccion5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+            double radioAr, baseAr, alturaAr, ladoAr, area;
+            byte opcion;
+
+            //Mostramos las opciones
+            Console.WriteLine("1. Círculo");
+            Console.WriteLine("2. Triangulo");
+            Console.WriteLine("3. Cuadrado");
+
+            //Preguntamos
+            Console.Write("Escoge una opción y calcularé su área: ");
+            opcion = Convert.ToByte(Console.ReadLine());
+
+            switch(opcion)
+            {
+                case 1:
+                    Console.Write("Dame el valor del radio de tu círculo: ");
+                    radioAr = Convert.ToDouble(Console.ReadLine());
+
+                    //Invocamos al método
+                    area = Circulo(radioAr);
+
+                    //Mostramos el resultado
+                    Console.WriteLine("El área es: {0}", area);
+                    break;
+                case 2:
+                    //Pedimos los valores de base y altura
+                    Console.Write("Dame el valor de la base de tu triangulo: ");
+                    baseAr = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Dame el valor de la altura de tu triangulo: ");
+                    alturaAr = Convert.ToDouble(Console.ReadLine());
+
+                    //Invocamos al método
+                    area = Triangulo(baseAr, alturaAr);
+
+                    //Mostramos el resultado
+                    Console.WriteLine("El área es: {0}", area);
+                    break;
+                case 3:
+                    Console.Write("Dame el valor de uno de los lados de tu cuadrado: ");
+                    ladoAr = Convert.ToDouble(Console.ReadLine());
+
+                    area = Cuadrado(ladoAr);
+
+                    //Mostramos el resultado
+                    Console.WriteLine("El área es: {0}", area);
+                    break;
+            }
+        }
+
+        //Método que calcula el área de un círculo
+        static double Circulo(double radioPa)
+        {
+            double area;
+
+            //Calcula el área
+            area = Math.PI * (radioPa * radioPa);
+
+            //Regresamos el valor de área
+            return area;
+        }
+
+        //Método que calcula el área de un triangulo
+        static double Triangulo(double basePa, double alturaPa)
+        {
+            double area;
+
+            //Calcula el área
+            area = (basePa * alturaPa) / 2;
+
+            //Regresamos el valor de área
+            return area;
+        }
+
+        //Método que calcula el área de un cuadrado
+        static double Cuadrado(double ladoPa)
+        {
+            double area;
+
+            //Calculamos el área
+            area = ladoPa * ladoPa;
+
+            //Regresamos el valor del área
+            return area;
+        }
+    }
+}
+ */
