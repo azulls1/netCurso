092407fdf6c00591f38dd019dc4079974f78e052
Samael Hernandez <azull.samael@gmail.com>
2024-05-15 16:07:28 -0600

    15 / 05 / 2024

--- seccion7_clases/Seccion7.2_Acceder_campos_clase/Seccion7.2_Acceder_campos_clase/Program.cs
+++ seccion7_clases/Seccion7.2_Acceder_campos_clase/Seccion7.2_Acceder_campos_clase/Program.cs
@@ -1,30 +1,29 @@
-﻿ using System;
-using System.CodeDom;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace seccion7._1_Declaracion_de_una_clase
+namespace Seccion7._2_Acceder_campos_clase
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
             //recomendacion repasar el programa
             //seccion5.1_metodos_otimizados
             //seccion5.2_pasar_x_ref_vs_pasar_x_val
 
             //instanciando a la clase automovil
             //el lado izquiero es la referencia  //el lado derecho crea el tipo de objeto
-                          automovil automovil1 = new automovil();
+            automovil automovil1 = new automovil();
             automovil1.Acelerar();
-            
-        } // fin del MAIN
-        
-    }//fin de la clase program
+        }
 
-    
+
+    }
+
     //[modificador de acceso] [class] [identificador]
     public class automovil
     {
@@ -64,6 +63,8 @@ namespace seccion7._1_Declaracion_de_una_clase
 
 
     }
+
+
 
 
 }
